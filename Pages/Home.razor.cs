ef46af5339c00edc322bc8315a096cc701e74ec9
Benjamin Vertonghen <benjamin.vertonghen@append.be>
2024-03-08 11:05:23 +0100

    Add Logging on page

---
+++ Pages/Home.razor.cs
@@ -0,0 +1,29 @@
+﻿using BlazorApplicationInsights.Interfaces;
+using BlazorApplicationInsights.Models;
+using Microsoft.AspNetCore.Components;
+using Serilog;
+
+namespace AnalyticsBug.Pages;
+
+public partial class Home
+{
+    [Inject] public required IApplicationInsights AppInsights { get; set; }
+
+    protected override void OnInitialized()
+    {
+        Log.Information("Hello from Home");
+        Log.Warning("Warning");
+        Log.Error("Error");
+        Log.Fatal("Fatal");
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            await AppInsights.TrackEvent(new EventTelemetry { Name = "FeaturePage" });
+            await AppInsights.TrackMetric(new MetricTelemetry { Name = "FeaturePage", Average = 1 });
+        }
+    }
+}
+
